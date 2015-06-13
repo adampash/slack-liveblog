2608f10b0d678f37b57e9b0b8a0812024317aa30
Adam Pash <adam.pash@gmail.com>
2015-06-13 19:41:29 -0400

    Setting surrogate keys

--- app/controllers/live_blogs_controller.rb
+++ app/controllers/live_blogs_controller.rb
@@ -4,16 +4,20 @@ class LiveBlogsController < ApplicationController
 
   def show
     @live_blog = LiveBlog.find params[:id]
+    set_surrogate_key_header @live_blog.record_key
   end
 
   def latest
     live_blog = LiveBlog.find params[:id]
     @messages = live_blog.latest_messages
+    set_surrogate_key_header [live_blog.record_key, @messages.map(&:record_key)].flatten
+
   end
 
   def cursor
     live_blog = LiveBlog.find params[:id]
     @messages = live_blog.from_cursor(params[:cursor])
+    set_surrogate_key_header @messages.map(&:record_key)
   end
 
   private
