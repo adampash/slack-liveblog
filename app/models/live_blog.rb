9c94a6e0d23f65ddc2d98eec923adaacd2a2da32
Adam Pash <adam.pash@gmail.com>
2015-06-15 11:04:18 -0400

    logging for cache

--- app/models/live_blog.rb
+++ app/models/live_blog.rb
@@ -24,8 +24,10 @@ class LiveBlog < ActiveRecord::Base
   def self.find_cache(id)
     in_cache = Rails.cache.read("live_blog/#{id}")
     if in_cache
+      puts "CACHED"
       live_blog = in_cache
     else
+      puts "NOT CACHED"
       live_blog = LiveBlog.find(id)
       Rails.cache.write(
         "live_blog/#{id}",
@@ -47,8 +49,10 @@ class LiveBlog < ActiveRecord::Base
   def latest_messages_cache(count)
     in_cache = Rails.cache.read("latest/#{count}")
     if in_cache
+      puts "CACHED"
       messages = in_cache
     else
+      puts "NOT CACHED"
       messages = latest_messages(count)
       Rails.cache.write(
         "latest_messages/#{count}",
